18ff71cb1eb324714196bcdb5225225feb0caeeb
pixelstudi07 <pixelversestudi07@gmail.com>
2025-04-15 18:00:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
 
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("show");
+});
