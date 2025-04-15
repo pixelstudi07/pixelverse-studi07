c9862b9feff6f154119d114aa4b881fee0b872a9
pixelstudi07 <pixelversestudi07@gmail.com>
2025-04-15 18:13:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("show");
-});
+// Currently empty - animations handled with CSS
+// Future interactivity can be added here
+console.log("PixelVerse Studi07 loaded");
